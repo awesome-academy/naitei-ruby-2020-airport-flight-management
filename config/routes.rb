b90be4aed163edd95299b5ef835dabd1964396b6
Tran Huy Hoang <tran.huy.hoangb@sun-asterisk.com>
2020-09-04 16:17:29 +0700

    Task/26978-admin-QLtk-nvkhongluu

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
     resources :requests, except: %i(show edit destroy)
     resources :schedules, except: %i(new create destroy)
     resources :notifications, only: %i(index destroy)
-
     devise_for :users
-
-    resources :users, only: :show
+    resources :users, only: %i(show edit update )
+    resources :pilots, only: :index
   end
 end
